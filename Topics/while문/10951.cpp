5efd31d125eb164dadf5988082c3425d2d5087ed
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-27 23:24:30 +0900

    while문 - (10951) A+B - 5

---
+++ Topics/while문/10951.cpp
@@ -0,0 +1,22 @@
+/*
+ * 백준 - 단계별 문제풀이 : while문
+ * 10951번
+ *
+ * A+B - 4
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *
+ */
+
+#include <iostream>
+
+int main() {
+
+    int A, B;
+
+    while (std::cin >> A >> B) {
+
+        std::cout << A + B << std::endl;
+
+    }
+
+}
