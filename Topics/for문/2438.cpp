dcf740daf72592fa9b040f76242ae9158766fae5
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-27 21:26:02 +0900

    for문 - (2438) 별 찍기 - 1

---
+++ Topics/for문/2438.cpp
@@ -0,0 +1,29 @@
+/*
+ * 백준 - 단계별 문제풀이 : for문
+ * 2438번
+ *
+ * 별 찍기 - 1
+ * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+ *
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
