0648720d173a72f4cae271e5aae3146a13eb05a8
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-27 15:06:21 +0900

    if문 - (1330) 두 수 비교하기

--- Topics/if문/1330.cpp
+++ Topics/if문/1330.cpp
@@ -3,14 +3,34 @@
  * 1330번
  *
  * 두 수 비교하기
+ * 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
  *
  */
 
 #include <iostream>
+#include <string> // string 사용을 위해
+
+std::string solution(int A, int B) {
+
+    if (A > B)
+        return ">";
+    else if (A < B)
+        return "<";
+    else
+        return "==";
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // 두 정수 A,B 입력
+    int A, B;
+    std::cin >> A >> B;
+
+
+    std::cout << solution(A, B) << std::endl;
     return 0;
 }
+
+
 
 
