60f0ec3a4fe52ba4a4c0c30e51934a1a24931f89
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-27 17:17:44 +0900

    for문 - (10950) A+B - 3

--- Topics/for문/10950.cpp
+++ Topics/for문/10950.cpp
@@ -1,4 +1,34 @@
-//
-// Created by 이유정 on 2021/08/27.
-//
+/*
+ * 백준 - 단계별 문제풀이 : for문
+ * 10950번
+ *
+ * A+B - 3
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *
+ */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    int n, a, b;
+
+    vector<int> v;
+
+    // 몇회 반복할지
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        v.push_back(a + b);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
