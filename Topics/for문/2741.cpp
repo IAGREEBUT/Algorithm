69dea074d3c156a1edb75a77521ce8562ea58207
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-27 17:50:26 +0900

    for문 - (2741)N찍기

--- Topics/for문/2741.cpp
+++ Topics/for문/2741.cpp
@@ -1,4 +1,23 @@
-//
-// Created by 이유정 on 2021/08/27.
-//
+/*
+ * 백준 - 단계별 문제풀이 : for문
+ * 2741번
+ *
+ * N 찍기
+ * 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+ *
+ */
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << "\n";
+    }
+
+}
