c5896c426e2891a082b734b8d3742d11cafcdb0c
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-27 17:04:50 +0900

    for문 - (2739) 구구단

--- Topics/for문/2739.cpp
+++ Topics/for문/2739.cpp
@@ -1,10 +1,33 @@
-//
-// Created by 이유정 on 2021/08/27.
-//
+/*
+ * 백준 - 단계별 문제풀이 : for문
+ * 2739번
+ *
+ * 구구단
+ * N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오.
+ *
+ */
 
 #include <iostream>
 
+void solution(int n) {
+
+    int res;
+
+    for (int i = 1; i < 10; i++) {
+        res = n * i;
+        std::cout << n << " * " << i << " = " << res << std::endl;
+    }
+
+
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // n단 입력받기
+    int n;
+    std::cin >> n;
+
+    solution(n);
+
     return 0;
 }
