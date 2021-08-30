8cfcf519662aacee6f1db6ff6438f2fe85e8ab4b
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-31 02:04:56 +0900

    기본수학1 - (10757) 큰 수 A + B

--- Topics/기본수학1/10757.cpp
+++ Topics/기본수학1/10757.cpp
@@ -11,7 +11,6 @@
  */
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 
@@ -19,71 +18,50 @@ using namespace std;
 
 
 int main() {
-//    long long a, b;
 
 
     string A, B;
     cin >> A >> B;
     vector<int> C;
-//    A = to_string(a);
-//    B = to_string(b);
 
+    //string 특성 상 .at 을 사용하면 앞자리 부터 되어서 반전
     reverse(A.begin(), A.end());
     reverse(B.begin(), B.end());
 
-//    cout << A << "\n";
-//    cout << B << "\n";
+
     int Alen, Blen;
-
-    int aa, bb, add_tmp, tmp;
-
     Alen = A.length();
     Blen = B.length();
 
+    int aa, bb, add_tmp, tmp;
     bool flag = false;
+
+
     if (Alen >= Blen) {
 
         for (int i = 0; i < Blen; i++) {
-//            int aa, bb;
-            aa = A.at(i) - '0';
+
+            aa = A.at(i) - '0'; //char into int
             bb = B.at(i) - '0';
 
-//            cout << "aa : " << aa << "bb :" << bb << "\n";
             tmp = aa + bb;
 
-//            cout << "tmp : " << tmp << "\n";
-
-//            int add_tmp;
-
-
-//            if (flag == true) C.push_back(tmp % 10 + 1);
-//            else C.push_back(tmp % 10);
-
+            //carry 존재 여부 반영
             if (flag == true) add_tmp = tmp + 1;
             else add_tmp = tmp;
 
+            // carry 발생 여부 확인
             if (add_tmp >= 10) flag = true;
             else flag = false;
 
             C.push_back(add_tmp % 10);
-//            cout << "flag : " << flag << "\n";
 
         }
 
+        //남은 자리수 계산
         for (int k = Blen; k < Alen; k++) {
             int tmp = A.at(k) - '0';
 
-//            cout << "2tmp : " << tmp << "\n";
-//            cout << "(tmp % 10 + 1) : " << tmp % 10 + 1 << "\n";
-//            cout << "tmp % 10 : " << tmp % 10 << "\n";
-//            cout << flag << "\n";
-
-//            if (flag == true) C.push_back(tmp % 10 + 1);
-//            else C.push_back(tmp % 10);
-//
-//            if (tmp >= 10) flag = true;
-//            else flag = false;
-
             if (flag == true) add_tmp = tmp + 1;
             else add_tmp = tmp;
 
@@ -92,29 +70,20 @@ int main() {
 
             C.push_back(add_tmp % 10);
 
-
         }
 
+        // 계산 후 마지막 남은 carry반영
         if (flag == true) C.push_back(1);
 
     } else {
 
 
         for (int i = 0; i < Alen; i++) {
-//            int aa, bb;
             aa = A.at(i) - '0';
             bb = B.at(i) - '0';
 
-//            cout << "aa : " << aa << "bb :" << bb << "\n";
             tmp = aa + bb;
 
-//            cout << "tmp : " << tmp << "\n";
-
-//            int add_tmp;
-
-
-//            if (flag == true) C.push_back(tmp % 10 + 1);
-//            else C.push_back(tmp % 10);
 
             if (flag == true) add_tmp = tmp + 1;
             else add_tmp = tmp;
@@ -123,23 +92,12 @@ int main() {
             else flag = false;
 
             C.push_back(add_tmp % 10);
-//            cout << "flag : " << flag << "\n";
 
         }
 
         for (int k = Alen; k < Blen; k++) {
             int tmp = B.at(k) - '0';
 
-//            cout << "2tmp : " << tmp << "\n";
-//            cout << "(tmp % 10 + 1) : " << tmp % 10 + 1 << "\n";
-//            cout << "tmp % 10 : " << tmp % 10 << "\n";
-//            cout << flag << "\n";
-
-//            if (flag == true) C.push_back(tmp % 10 + 1);
-//            else C.push_back(tmp % 10);
-//
-//            if (tmp >= 10) flag = true;
-//            else flag = false;
 
             if (flag == true) add_tmp = tmp + 1;
             else add_tmp = tmp;
@@ -160,7 +118,7 @@ int main() {
 
 
 
-    //출력
+    //출력 뒤에서 부터
     for (int j = C.size() - 1; j >= 0; j--) {
 
         cout << C[j];
