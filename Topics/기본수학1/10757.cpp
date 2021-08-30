8752877e7786e74af6fcbdf2569a74b68a60f22a
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-31 02:02:00 +0900

    기본수학1 - (10757) 큰 수 A + B _ 맞는데 너무 김

--- Topics/기본수학1/10757.cpp
+++ Topics/기본수학1/10757.cpp
@@ -13,19 +13,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 
 using namespace std;
 
 
 int main() {
-    long long a, b;
-    cin >> a >> b;
+//    long long a, b;
+
 
     string A, B;
+    cin >> A >> B;
     vector<int> C;
-    A = to_string(a);
-    B = to_string(b);
+//    A = to_string(a);
+//    B = to_string(b);
 
     reverse(A.begin(), A.end());
     reverse(B.begin(), B.end());
@@ -65,17 +66,17 @@ int main() {
             else flag = false;
 
             C.push_back(add_tmp % 10);
-            cout << "flag : " << flag << "\n";
+//            cout << "flag : " << flag << "\n";
 
         }
 
         for (int k = Blen; k < Alen; k++) {
             int tmp = A.at(k) - '0';
 
-            cout << "2tmp : " << tmp << "\n";
-            cout << "(tmp % 10 + 1) : " << tmp % 10 + 1 << "\n";
-            cout << "tmp % 10 : " << tmp % 10 << "\n";
-            cout << flag << "\n";
+//            cout << "2tmp : " << tmp << "\n";
+//            cout << "(tmp % 10 + 1) : " << tmp % 10 + 1 << "\n";
+//            cout << "tmp % 10 : " << tmp % 10 << "\n";
+//            cout << flag << "\n";
 
 //            if (flag == true) C.push_back(tmp % 10 + 1);
 //            else C.push_back(tmp % 10);
@@ -96,8 +97,70 @@ int main() {
 
         if (flag == true) C.push_back(1);
 
+    } else {
+
+
+        for (int i = 0; i < Alen; i++) {
+//            int aa, bb;
+            aa = A.at(i) - '0';
+            bb = B.at(i) - '0';
+
+//            cout << "aa : " << aa << "bb :" << bb << "\n";
+            tmp = aa + bb;
+
+//            cout << "tmp : " << tmp << "\n";
+
+//            int add_tmp;
+
+
+//            if (flag == true) C.push_back(tmp % 10 + 1);
+//            else C.push_back(tmp % 10);
+
+            if (flag == true) add_tmp = tmp + 1;
+            else add_tmp = tmp;
+
+            if (add_tmp >= 10) flag = true;
+            else flag = false;
+
+            C.push_back(add_tmp % 10);
+//            cout << "flag : " << flag << "\n";
+
+        }
+
+        for (int k = Alen; k < Blen; k++) {
+            int tmp = B.at(k) - '0';
+
+//            cout << "2tmp : " << tmp << "\n";
+//            cout << "(tmp % 10 + 1) : " << tmp % 10 + 1 << "\n";
+//            cout << "tmp % 10 : " << tmp % 10 << "\n";
+//            cout << flag << "\n";
+
+//            if (flag == true) C.push_back(tmp % 10 + 1);
+//            else C.push_back(tmp % 10);
+//
+//            if (tmp >= 10) flag = true;
+//            else flag = false;
+
+            if (flag == true) add_tmp = tmp + 1;
+            else add_tmp = tmp;
+
+            if (add_tmp >= 10) flag = true;
+            else flag = false;
+
+            C.push_back(add_tmp % 10);
+
+
+        }
+
+        if (flag == true) C.push_back(1);
+
+
     }
 
+
+
+
+    //출력
     for (int j = C.size() - 1; j >= 0; j--) {
 
         cout << C[j];
