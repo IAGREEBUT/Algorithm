f442c4dc5be6b2e9a5075d93d4dc7471d09ac048
YujeongLEE <dldbwjd24@ewhain.net>
2021-08-31 01:54:51 +0900

    기본수학1 - (10757) 큰 수 A + B _맞는것 같은데 확인필요_그리고 너무김

--- Topics/기본수학1/10757.cpp
+++ Topics/기본수학1/10757.cpp
@@ -30,32 +30,42 @@ int main() {
     reverse(A.begin(), A.end());
     reverse(B.begin(), B.end());
 
-    cout << A << "\n";
-    cout << B << "\n";
+//    cout << A << "\n";
+//    cout << B << "\n";
     int Alen, Blen;
+
+    int aa, bb, add_tmp, tmp;
 
     Alen = A.length();
     Blen = B.length();
 
-
+    bool flag = false;
     if (Alen >= Blen) {
 
-        bool flag = false;
         for (int i = 0; i < Blen; i++) {
-            int aa, bb;
+//            int aa, bb;
             aa = A.at(i) - '0';
             bb = B.at(i) - '0';
 
-            cout << "aa : " << aa << "bb :" << bb << "\n";
-            int tmp = aa + bb;
+//            cout << "aa : " << aa << "bb :" << bb << "\n";
+            tmp = aa + bb;
 
-            cout << "tmp : " << tmp << "\n";
+//            cout << "tmp : " << tmp << "\n";
 
-            if (flag == true) C.push_back(tmp % 10 + 1);
-            else C.push_back(tmp % 10);
+//            int add_tmp;
 
-            if (tmp >= 10) flag = true;
+
+//            if (flag == true) C.push_back(tmp % 10 + 1);
+//            else C.push_back(tmp % 10);
+
+            if (flag == true) add_tmp = tmp + 1;
+            else add_tmp = tmp;
+
+            if (add_tmp >= 10) flag = true;
             else flag = false;
+
+            C.push_back(add_tmp % 10);
+            cout << "flag : " << flag << "\n";
 
         }
 
@@ -65,19 +75,30 @@ int main() {
             cout << "2tmp : " << tmp << "\n";
             cout << "(tmp % 10 + 1) : " << tmp % 10 + 1 << "\n";
             cout << "tmp % 10 : " << tmp % 10 << "\n";
+            cout << flag << "\n";
 
-            if (flag == true) C.push_back(tmp % 10 + 1);
-            else C.push_back(tmp % 10);
+//            if (flag == true) C.push_back(tmp % 10 + 1);
+//            else C.push_back(tmp % 10);
+//
+//            if (tmp >= 10) flag = true;
+//            else flag = false;
 
-            if (tmp >= 10) flag = true;
+            if (flag == true) add_tmp = tmp + 1;
+            else add_tmp = tmp;
+
+            if (add_tmp >= 10) flag = true;
             else flag = false;
+
+            C.push_back(add_tmp % 10);
 
 
         }
 
+        if (flag == true) C.push_back(1);
+
     }
 
-    for (int j = 0; j < C.size(); j++) {
+    for (int j = C.size() - 1; j >= 0; j--) {
 
         cout << C[j];
 
