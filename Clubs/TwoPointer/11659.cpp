10fcab2c5f09bc90346ed00e5073c1f646145d52
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-15 17:18:43 +0900

    TwoPointer - 구간합 예제
    풀이2

--- Clubs/TwoPointer/11659.cpp
+++ Clubs/TwoPointer/11659.cpp
@@ -57,3 +57,35 @@ int main() {
 
 
 }
+
+
+// 피드백
+// 1. 인덱스 1부터 시작해서 예외를 없애는 게 좋음
+// 2. 배열 2개쓰지말고 그냥 동시에 처리해서 저장하는게 좋음
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(NULL);
+//
+//    int n, m, num;
+//
+//    //입력
+//    cin >> n >> m;
+//    vector<int> sum(n + 1, 0);
+//    for (int i = 1; i <= n; i++) { //인덱스를 1부터 시작하면 i=0인 경우를 따로 처리할 필요 없음
+//        cin >> num;
+//        sum[i] = sum[i - 1] + num; //첫번째 수부터 i번째 수까지의 누적합
+//    }
+//
+//    while (m--) {
+//        int s, e;
+//        cin >> s >> e;
+//
+//        //출력
+//        cout << sum[e] - sum[s - 1] << '\n';
+//    }
+//}
