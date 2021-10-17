265b512caac93c05175dfcbc9320f2b855c6b200
YujeongLEE <dldbwjd24@ewhain.net>
2021-10-17 23:53:39 +0900

    TwoPointer - 투 포인터(같은 위치부터 탐색)
    2020카카오 - 보석쇼핑

---
+++ Clubs/TwoPointer/jewelShopping.cpp
@@ -0,0 +1,21 @@
+//
+// 2020 카카오 인턴십 - 보석쇼핑
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+    vector<int> answer;
+    return answer;
+}
+
+
+int main() {
+
+    vector<string> gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+
+    solution(gems);
+}
