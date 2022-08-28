0efe99d7826426616627ecb2a2098e0c11ef76b2
YujeongLEE <dldbwjd24@ewhain.net>
2022-08-29 00:15:06 +0900

    (1546번) 단계별 - 1차원 배열
    상대오차에 대한 내용

--- Baekjoon/1차원 배열/1546.cpp
+++ Baekjoon/1차원 배열/1546.cpp
@@ -2,3 +2,34 @@
 // Created by 이유정 on 2022/08/28.
 //
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<double> arr;
+    arr.assign(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    double max = arr[n - 1];
+
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i] / max * 100; //double 넣어주지 않으면 0 이하는 다 0처리! 명심
+    }
+
+    cout.precision(15); //상대 오차에 대한 내용
+    cout << (double) sum / n;
+
+
+}
